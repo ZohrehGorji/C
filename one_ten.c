18fa8e43ad2371ab84c4ed6f3a47b3362edee33c
Zohreh Gorji <mily_milly90@yahoo.com>
2017-08-31 18:09:41 +0200

    Add files via upload

---
+++ one_ten.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  one_ten
+//
+//  Created by Zohreh Gorji on 8/31/17.
+//  Copyright © 2017 Zohreh Gorji. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    for (int i=0; i<10; i++) {
+        printf("%d\n",i);
+        
+    }
+    return 0;
+}
